6dd649c59722f6d4715887b629c0dc2955b92d5b
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2019-10-15 11:02:53 +0200

    support synchronizing any branch

---
+++ commit_sync_public_branch.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+##############################################################################################
+##
+## This script is intended to be run on a clone of a private repository such as
+## SonarSource/sonar-enterprise or slang-enterprise
+##
+## This script commits work of init_branches.sh and sync_public_branch.sh by pushing:
+##  1- branch public_[branch_name] to private repository's public_[branch_name]
+##  2- branch public_[branch_name] to public repository's [branch_name]
+##  3- the references used by sync_public_branch.sh to the private repository
+##
+## parameters: 
+## PUBLIC_REMOTE: slang or sq
+## BRANCH: master, branch-7.9, ...
+##
+##############################################################################################
+
+set -euo pipefail
+
+PUBLIC_REMOTE="${1}"
+PRIVATE_REMOTE="origin"
+REF_TREE_ROOT="refs/public_sync"
+BRANCH="${2}"
+PUBLIC_BRANCH="public_${BRANCH}"
+
+info() {
+  local message="$1"
+  echo "[INFO] ${message}"
+}
+
+error() {
+  local message="$1"
+  echo "[ERROR] ${message}"
+}
+
+pause() {
+  echo "pause..."
+  read
+}
+
+# to know where we are
+git checkout "${PUBLIC_BRANCH}"
+
+info "Pushing ${PUBLIC_BRANCH}..."
+git push "${PRIVATE_REMOTE}" "${PUBLIC_BRANCH}:${PUBLIC_BRANCH}"
+git push "${PUBLIC_REMOTE}" "${PUBLIC_BRANCH}:${BRANCH}"
+
+info "Pushing refs to ${PUBLIC_REMOTE}..."
+TMP_EXISTING_REFS_FILE=$(mktemp)
+git ls-remote "${PRIVATE_REMOTE}" | cut -f 2 | grep "^${REF_TREE_ROOT}/" > ${TMP_EXISTING_REFS_FILE} || true
+
+for ref in $(git for-each-ref "${REF_TREE_ROOT}" | cut -f 2 | grep -v --file="${TMP_EXISTING_REFS_FILE}"); do
+  echo "committing ref $ref"
+  git push "${PRIVATE_REMOTE}" "${ref}"
+done
+
+info "done"
--- init_branches.sh
+++ init_branches.sh
@@ -13,6 +13,7 @@
 ## parameters: 
 ## PUBLIC_REMOTE: slang or sq
 ## PUBLIC_REMOTE_URL: git@github.com:SonarSource/slang.git or git@github.com:SonarSource/sonarqube.git
+## BRANCH: master, branch-7.9, ...
 ##
 ##############################################################################################
 
@@ -20,18 +21,20 @@
 set -euo pipefail
 
 info() {
-  local MESSAGE="$1"
-  echo "[INFO] ${MESSAGE}"
+  local message="$1"
+  echo "[INFO] ${message}"
 }
 
 error() {
-  local MESSAGE="$1"
-  echo "[ERROR] ${MESSAGE}"
+  local message="$1"
+  echo "[ERROR] ${message}"
 }
 
 PRIVATE_REMOTE="origin"
-PUBLIC_REMOTE=$1
-PUBLIC_REMOTE_URL=$2
+PUBLIC_REMOTE="${1}"
+PUBLIC_REMOTE_URL="${2}"
+BRANCH="${3}"
+PUBLIC_BRANCH="public_${BRANCH}"
 REF_TREE_ROOT="refs/public_sync"
 # in branch master_public, created from SonarSource/slang master
 PUBLIC_PROJECT_HEAD_SHA1="88b6f0111c4bfdb243b9beaa531a6c7a49ac30b0"
@@ -40,8 +43,10 @@ PROJECT_MERGE_COMMIT_SHA1="d169e263121b02a1332e8936793705820c7a10d3"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
+info "Initializing branch ${BRANCH} for remote ${PUBLIC_REMOTE} from repository ${PUBLIC_REMOTE_URL}..."
+
 # to know where we are
-git checkout master
+git checkout "${BRANCH}"
 
 info "Syncing refs from remote..."
 git fetch "${PRIVATE_REMOTE}" "+${REF_TREE_ROOT}/*:${REF_TREE_ROOT}/*"
@@ -51,10 +56,10 @@ if ! git remote | grep -qxF "${PUBLIC_REMOTE}"; then
   git remote add "${PUBLIC_REMOTE}" "${PUBLIC_REMOTE_URL}"
 fi
 
-# create "public_master" if doesn't exist yet
-if [ -z "$(git branch --list "public_master")" ]; then
-  info "create branch public_master from ${PUBLIC_PROJECT_HEAD_SHA1}"
-  git checkout -b "public_master" "${PUBLIC_PROJECT_HEAD_SHA1}"
+# create "${PUBLIC_BRANCH}" if doesn't exist yet
+if [ -z "$(git branch --list "${PUBLIC_BRANCH}")" ]; then
+  info "create branch "${PUBLIC_BRANCH}" from ${PUBLIC_PROJECT_HEAD_SHA1}"
+  git checkout -b "${PUBLIC_BRANCH}" "${PUBLIC_PROJECT_HEAD_SHA1}"
 fi
 
 # fail if already initialized
@@ -65,8 +70,8 @@ if [ "$(git for-each-ref --count=1 "${REF_TREE_ROOT}")" ]; then
 fi
 
 info "create inital refs"
-git update-ref "${REF_TREE_ROOT}/${TIMESTAMP}/master" "${PROJECT_MERGE_COMMIT_SHA1}"
-git update-ref "${REF_TREE_ROOT}/${TIMESTAMP}/public_master" "${PUBLIC_PROJECT_HEAD_SHA1}"
+git update-ref "${REF_TREE_ROOT}/${TIMESTAMP}/${BRANCH}" "${PROJECT_MERGE_COMMIT_SHA1}"
+git update-ref "${REF_TREE_ROOT}/${TIMESTAMP}/${PUBLIC_BRANCH}" "${PUBLIC_PROJECT_HEAD_SHA1}"
 
 info "done"
 
